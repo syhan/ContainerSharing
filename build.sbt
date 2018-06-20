8988d2ad9093b88a245dd71982a376f43410a067
syhan <syhannnn@gmail.com>
2018-06-20 21:06:16 +0800

    Initial commit for data extract from different sources

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "Container"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.4.0"
+libraryDependencies += "org.elasticsearch" % "elasticsearch-hadoop" % "6.2.1"
+libraryDependencies += "com.typesafe.play" %%  "play-json" % "2.4.11"
+//libraryDependencies += "io.spray" %%  "spray-json" % "1.3.3"
+
+
---
+++ src/main/scala/com/sap/seawide/ContainerExtractor.scala
@@ -0,0 +1,89 @@
+
+package com.sap.seawide
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.elasticsearch.spark._
+import play.api.libs.json.Json
+import scalaj.http.{Http, HttpResponse}
+
+import scala.util.Random
+
+
+object ContainerExtractor {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName("Container Extractor")
+      .config("es.index.auto.create", "true")
+      .config("es.input.json", "true")
+      .config("es.batch.size.entries", "8")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+
+    maersk(sc)
+  }
+
+  def cosco(sc: SparkContext): Unit = {
+    vendor(sc, Seq("CCLU"), "http://elines.coscoshipping.com/ebtracking/public/containers/")
+      .map(r => Json.parse(r.body))
+      .filter(json => (json \ "code").as[String].eq("200") && (json \ "data" \ "content" \ "notFound").as[String].isEmpty)
+      .map(Json.stringify)
+      .saveToEs("containers/cosco")
+  }
+
+  def maersk(sc: SparkContext): Unit = {
+    vendor(sc, Seq("MSKU"), "https://api.maerskline.com/track/")
+      .filter(_.is2xx)
+      .map(_.body)
+      .saveToEs("containers/maersk")
+    //      .foreach(println)
+  }
+
+  def valid(num: String): Boolean = { // https://en.wikipedia.org/wiki/ISO_6346
+    val dict = "0123456789A?BCDEFGHIJK?LMNOPQRSTU?VWXYZ"
+
+    val sum = num.zipWithIndex.slice(0, 10).foldLeft(0d)((acc, t) => acc + Math.pow(2, t._2) * dict.indexOf(t._1))
+
+    dict.indexOf(num.charAt(10)) == ((sum % 11) % 10)
+  }
+
+  def serialNumber(sc: SparkContext, range: (Int, Int), prefix: Seq[String]): RDD[String] = {
+    sc.parallelize(Range(range._1, range._2)).flatMap(s => prefix.map(p => "%s%07d".format(p, s))).filter(valid)
+  }
+
+  def vendor(sc: SparkContext, prefix: Seq[String], url: String): RDD[HttpResponse[String]] = {
+    //    val range = 1000000
+    val range = 5005500
+
+    serialNumber(sc, (5005000, range), prefix).map(number => {
+      val proxy = randomProxy(number)
+
+      Thread.sleep(1000) // be polite and not too aggressive
+
+      println(s"Querying $number with $proxy...")
+
+      Http(s"$url$number")
+        .proxy(proxy, 8080)
+        .timeout(50000, 50000)
+        .asString
+    })
+  }
+
+  def randomProxy(num: String): String = {
+    val domains: List[String] = List(/*"wdf", "pal", "sin", "pvgl", "pek", */ "hkg", "tyo", "nyc", "bos", "sfo", "phl" /*, "man", "muc", "fra", "ber", "vie"*/)
+    val seed = num.substring(5).toInt * new Random().nextInt(1000)
+    val domain = domains(seed % domains.size)
+
+    s"proxy.$domain.sap.corp"
+  }
+
+  def blacklist(number: String, domain: String): Unit = {
+
+  }
+
+}
